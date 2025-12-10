b4fa74a78a7dd522b41e51cc882b3daf74a2d2c4
ademocel <ademocel83@gmail.com>
2025-12-10 18:29:30 +0300

    file-formated

--- src/SwiftLog.py
+++ src/SwiftLog.py
@@ -1,23 +1,31 @@
-from datetime import datetime
 import os
-class Logger():
-    def __init__(self,module_name:str):
-        self.module_name = module_name
-        os.makedirs('logs',exist_ok=True)
-        with open(f'logs/{self.module_name}.log','a',encoding='utf-8') as file:
-            file.write('')
-    def log(self,level_name:str,message:str):
-        with open(f'logs/{self.module_name}.log','a',encoding='utf-8') as file:
-            now:datetime = datetime.now()
-            log:str = f'[{now.strftime('%Y-%m-%d')}] [{now.strftime('%H:%M:%S')}] [{level_name}] [{self.module_name}]: {message}\n'
-            file.write(log)
-            print(log.removesuffix('\n'))
+from datetime import datetime
 
-    def INFO(self,message:str):
-        self.log('INFO',message)
-    def WARNING(self,message:str):
-        self.log('WARNING',message)
-    def ERROR(self,message:str):
-        self.log('ERROR',message)
-    def CRITICAL(self,message:str):
-        self.log('CRITICAL',message)
+
+class Logger:
+    def __init__(self, module_name: str):
+        self.module_name = module_name
+        os.makedirs("logs", exist_ok=True)
+        with open(f"logs/{self.module_name}.log", "a", encoding="utf-8") as file:
+            file.write("")
+
+    def log(self, level_name: str, message: str):
+        with open(f"logs/{self.module_name}.log", "a", encoding="utf-8") as file:
+            now: datetime = datetime.now()
+            log: str = (
+                f"[{now.strftime('%Y-%m-%d')}] [{now.strftime('%H:%M:%S')}] [{level_name}] [{self.module_name}]: {message}\n"
+            )
+            file.write(log)
+            print(log.removesuffix("\n"))
+
+    def INFO(self, message: str):
+        self.log("INFO", message)
+
+    def WARNING(self, message: str):
+        self.log("WARNING", message)
+
+    def ERROR(self, message: str):
+        self.log("ERROR", message)
+
+    def CRITICAL(self, message: str):
+        self.log("CRITICAL", message)
